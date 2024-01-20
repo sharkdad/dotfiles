36830b23c58ebf87365301c8d8dcbe873dc0b6df
sharkdad <46014430+sharkdad@users.noreply.github.com>
2024-01-20 14:09:06 -0800

    add early-init.el

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,10 @@
+(load-theme 'modus-vivendi t)
+
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+
+(setq frame-resize-pixelwise t)
+(setq visible-bell t)
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
